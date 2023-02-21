585f4f90309dae7fdb2496fbe4085bb8eadf56b0
Vamshi-Munukuntla <vamshi.kumar59@gmail.com>
2023-02-21 20:26:27 +0530

    Streamlit basics are completed.

---
+++ st_simple_2.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import plotly.express as px
+import streamlit as st
+import altair as alt
+
+
+@st.cache_data()
+def load_data():
+    src_file = 'EPA_fuel_economy_summary.csv'
+    raw_df = pd.read_csv(src_file)
+    return raw_df
+
+
+df = load_data()
+min_year = int(df['year'].min())
+max_year = int(df['year'].max())
+valid_makes = sorted(df['make'].unique())
+
+
+# Set up the UI
+st.title('Simple Example')
+make = st.multiselect('Select a make:', valid_makes)
+year_range = st.slider(
+    label='Year range',
+    min_value=min_year,
+    max_value=max_year,
+    value=(min_year, max_year)
+)
+
+# Filter data based on inputs
+year_filter = df['year'].between(year_range[0], year_range[1])
+make_filter = df['make'].isin(make)
+
+plot_df = df[make_filter & year_filter]
+
+avg_fuel_economy = plot_df['fuelCost08'].mean().round()
+st.metric('Average', avg_fuel_economy)
+
+# plot the data
+fig = px.histogram(
+    plot_df,
+    x='fuelCost08',
+    color='class_summary',
+    labels={'fuelCost08':'Annual Fuel Cost'},
+    nbins=40,
+    title='Fuel Cost Distribution'
+)
+
+altair_chart = (
+    alt.Chart(plot_df).mark_tick().encode(y='fuel_type_summary',
+                                          x='barrels08')
+)
+
+# Display the output results
+st.write(fig)
+st.write(altair_chart)
+
+st.write('Sample data', plot_df.head(10))
+
+
+
